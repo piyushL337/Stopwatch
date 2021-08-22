1464b068d78c4ebe09577eee5b5c227626dddc7e
Piyush Joshi <75676402+piyushL337@users.noreply.github.com>
2021-08-22 11:52:20 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,14 +34,11 @@ function timerCycle(){
     }
     setTimeout("timerCycle()", 1000);
 }
-$(document).ready(function()
-{
- if($("#foo").attr("href")!="https://lala.github.io/")
- {
-  window.location.href="https://piyushjoshi.tk";
- }
-
-});
+window.onload=function(){
+  var footer=document.getElementById("foo");
+      if(footer===null || footer===undefined)
+         window.location="https://piyushjoshi.tk";
+}
 function startTimer(){
     if (stoptime == true){
         stoptime = false;
